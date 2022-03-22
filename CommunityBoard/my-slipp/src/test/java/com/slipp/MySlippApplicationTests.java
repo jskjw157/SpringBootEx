4ab2dbc5379f4cecf922f4d63dcca6bf28c7efbd
johny <69754601+jskjw157@users.noreply.github.com>
2022-03-23 03:13:52 +0900

    start spring project (jquery, bootstrap)

---
+++ CommunityBoard/my-slipp/src/test/java/com/slipp/MySlippApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slipp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySlippApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
