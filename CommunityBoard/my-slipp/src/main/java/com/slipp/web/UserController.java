55a87e24927bc8f58cfc28cef71962462b38569f
johny <69754601+jskjw157@users.noreply.github.com>
2022-03-28 23:58:40 +0900

    add userController, user bean

---
+++ CommunityBoard/my-slipp/src/main/java/com/slipp/web/UserController.java
@@ -0,0 +1,16 @@
+package com.slipp.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+	
+	@PostMapping("/create")
+	public String create(User user) {
+		System.out.println("user" + user);
+		return "index";
+	}
+
+}
