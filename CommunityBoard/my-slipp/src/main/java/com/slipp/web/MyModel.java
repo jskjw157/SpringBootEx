cada3098a5612eb0cf5483f114653021f4e2873b
johny <69754601+jskjw157@users.noreply.github.com>
2022-04-05 02:27:56 +0900

    mustach 예제 추가2

---
+++ CommunityBoard/my-slipp/src/main/java/com/slipp/web/MyModel.java
@@ -0,0 +1,21 @@
+package com.slipp.web;
+
+public class MyModel {
+	
+	private String name;
+
+	public MyModel(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
