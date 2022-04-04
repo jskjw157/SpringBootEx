61b1e4af5818cbe59c03b6c59ea6b6884547aa60
johny <69754601+jskjw157@users.noreply.github.com>
2022-04-05 02:27:31 +0900

    mustache 예제 추가2

--- CommunityBoard/my-slipp/src/main/java/com/slipp/web/WelcomeController.java
+++ CommunityBoard/my-slipp/src/main/java/com/slipp/web/WelcomeController.java
@@ -1,5 +1,9 @@
 package com.slipp.web;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +13,8 @@ public class WelcomeController {
 	
 	@GetMapping("/helloworld")
 	public String welcome( Model model) {
-		model.addAttribute("name", "jiwon");
-		model.addAttribute("company", "<b>git</b>");
-		model.addAttribute("age", 30);
+		List<MyModel> repo = Arrays.asList(new MyModel("jiwon"),new MyModel("dahee"),new MyModel("javajigi"));
+		model.addAttribute("repo", repo);
 		return "welcome";
 	}
 	
