bc5be047036f9665c35ace6ff67cee1da5994ed7
johny <69754601+jskjw157@users.noreply.github.com>
2022-05-22 23:50:31 +0900

    JPA(dpendency 추가, properties 설정), User Class JPA로 매핑 , UserRepository 추가

--- CommunityBoard/my-slipp/src/main/java/com/slipp/domain/User.java
+++ CommunityBoard/my-slipp/src/main/java/com/slipp/domain/User.java
@@ -1,13 +1,21 @@
-package com.slipp.web;
+package com.slipp.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	@Id
+	@GeneratedValue
+	private Long id;
 	
+	@Column(nullable = false, length=20)
 	private String userId;
 	
 	private String password;
-
 	private String name;
-	
 	private String email;
 
 	public String getUserId() {
--- CommunityBoard/my-slipp/src/main/java/com/slipp/web/UserController.java
+++ CommunityBoard/my-slipp/src/main/java/com/slipp/web/UserController.java
@@ -3,29 +3,36 @@ package com.slipp.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.slipp.domain.User;
+import com.slipp.domain.UserRepository;
+
 @Controller
 public class UserController {
 	
+	@Autowired
+	private UserRepository userRepository;
+	
 	private List<User> users = new ArrayList<>();
 	
-	@PostMapping("/create")
+	@PostMapping("/user/create")
 	public String createUser(User user) {
 		System.out.println(user);
-		users.add(user);
+		userRepository.save(user);
 		return "redirect:/list";
 	}
 	
 	@GetMapping("/list")
 	public String userList(Model model) {
 		
-		model.addAttribute("users",users);
+		model.addAttribute("users",userRepository.findAll());
 		
-		return "list";
+		return "/user/list.html";
 	}
 	
 
