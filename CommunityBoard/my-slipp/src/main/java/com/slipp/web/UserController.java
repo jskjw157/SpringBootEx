a76e060d696543962548e79d9380be682bfa702b
johny <69754601+jskjw157@users.noreply.github.com>
2022-05-23 00:16:42 +0900

    UserController URL 설정

--- CommunityBoard/my-slipp/src/main/java/com/slipp/web/UserController.java
+++ CommunityBoard/my-slipp/src/main/java/com/slipp/web/UserController.java
@@ -8,31 +8,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.slipp.domain.User;
 import com.slipp.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
 	
-	private List<User> users = new ArrayList<>();
-	
-	@PostMapping("/user/create")
+	@PostMapping("")
 	public String createUser(User user) {
 		System.out.println(user);
 		userRepository.save(user);
-		return "redirect:/list";
+		return "redirect:/users";
 	}
 	
-	@GetMapping("/list")
+	@GetMapping("")
 	public String userList(Model model) {
 		
 		model.addAttribute("users",userRepository.findAll());
 		
-		return "/user/list.html";
+		return "/user/list";
 	}
 	
 
